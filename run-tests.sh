97ce3cb5d0fb1bf10ff2a103c544574e2bed9cfe
Emil Mikulic <emikulic@gmail.com>
2006-12-13 08:29:53 +0000

    Turn up the debugging and warnflags.

--- run-tests.sh
+++ run-tests.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+BDECFLAGS="-W -Wall -ansi -pedantic -Wbad-function-cast -Wcast-align \
+-Wcast-qual -Wchar-subscripts -Winline -Wmissing-prototypes -Wnested-externs \
+-Wpointer-arith -Wredundant-decls -Wshadow -Wstrict-prototypes -Wwrite-strings"
 
-(cc test_make_safe_uri.c -o test_make_safe_uri &&
+(cc $BDECFLAGS -g -O -DDEBUG test_make_safe_uri.c -o test_make_safe_uri &&
 ./test_make_safe_uri)
