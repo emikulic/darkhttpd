de03d2095853a73ad3a8c3025dfb53d9d87ea6e8
Emil Mikulic <emikulic@gmail.com>
2006-12-13 07:15:32 +0000

    Need some release engineering reminders after I failed to update the pkgname when I released v1.4.

---
+++ warnflags.sh
@@ -0,0 +1,5 @@
+BDECFLAGS="-W -Wall -ansi -pedantic -Wbad-function-cast -Wcast-align \
+-Wcast-qual -Wchar-subscripts -Winline -Wmissing-prototypes -Wnested-externs \
+-Wpointer-arith -Wredundant-decls -Wshadow -Wstrict-prototypes -Wwrite-strings"
+
+(cd trunk && rm -f darkhttpd && make bsd CFLAGS="$BDECFLAGS")
