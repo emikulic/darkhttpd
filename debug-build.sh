f29b75fb1db807f33b89d2174873045a13f09425
Emil Mikulic <emikulic@gmail.com>
2013-04-28 21:16:40 +1000

    Clean up and improve the devel/ scripts.

--- debug-build.sh
+++
@@ -1,7 +0,0 @@
-BDECFLAGS="-W -Wall -ansi -pedantic -Wbad-function-cast -Wcast-align \
--Wcast-qual -Wchar-subscripts -Winline -Wmissing-prototypes -Wnested-externs \
--Wpointer-arith -Wredundant-decls -Wshadow -Wstrict-prototypes -Wwrite-strings"
-
-(cd trunk &&
-rm -f darkhttpd &&
-make bsd CFLAGS="$BDECFLAGS -g -DDEBUG -DTORTURE")
