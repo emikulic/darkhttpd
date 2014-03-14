9fe3ed4fc179f39de7f932ea37156f085846c8e6
Emil Mikulic <emikulic@gmail.com>
2014-03-14 22:50:44 +1100

    Run test_make_safe_uri.  Retire broken run-tests.sh

--- run-tests.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-BDECFLAGS="-W -Wall -ansi -pedantic -Wbad-function-cast -Wcast-align \
--Wcast-qual -Wchar-subscripts -Winline -Wmissing-prototypes -Wnested-externs \
--Wpointer-arith -Wredundant-decls -Wshadow -Wstrict-prototypes -Wwrite-strings"
-
-(cc $BDECFLAGS -g -O -DDEBUG test_make_safe_uri.c -o test_make_safe_uri &&
-./test_make_safe_uri)
