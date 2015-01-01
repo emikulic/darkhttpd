72c55394123746db05537ef5bbdb6a1f6b0922b9
Emil Mikulic <emikulic@gmail.com>
2015-01-01 18:32:01 +1100

    When testing, build with -DDEBUG.

--- devel/fuzz.sh
+++ devel/fuzz.sh
@@ -2,6 +2,6 @@
 AFL_PATH=~/afl/afl-1.06b
 export AFL_PATH
 TMP=/dev/shm/darkhttpd
-AFL_HARDEN=1 $AFL_PATH/afl-gcc -O3 fuzz_make_safe_uri.c -o fuzz_make_safe_uri
+AFL_HARDEN=1 $AFL_PATH/afl-gcc -O3 -DDEBUG fuzz_make_safe_uri.c -o fuzz_make_safe_uri
 mkdir $TMP
 $AFL_PATH/afl-fuzz -i fuzz_testcases -o $TMP ./fuzz_make_safe_uri
