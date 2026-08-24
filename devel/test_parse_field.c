78600a728ceaab3fd851208678e89aecd6849747
Emil Mikulic <emikulic@gmail.com>
2026-08-24 12:48:26 +1000

    test_parse_field: no output for passing tests.

--- devel/test_parse_field.c
+++ devel/test_parse_field.c
@@ -2,35 +2,31 @@
 #include "../darkhttpd.c"
 #undef main
 
-static int ret = EXIT_SUCCESS;
-
 #define test(a, b) test_helper(__FILE__, __LINE__, a, b)
 
 static void test_helper(const char *file, int line, const char *expected,
                         const char *actual) {
   if (actual == NULL && expected == NULL) {
-    printf("%s:%d:PASS\n", file, line);
+    printf("%s:%d:PASS: NULL\n", file, line);
     return;
   }
   if (actual == NULL) {
     printf("%s:%d:FAIL: got NULL, expected \"%s\"\n", file, line, expected);
-    ret = EXIT_FAILURE;
     return;
   }
   if (expected == NULL) {
     printf("%s:%d:FAIL: got \"%s\", expected NULL\n", file, line, actual);
-    ret = EXIT_FAILURE;
     return;
   }
   if (strcmp(expected, actual) != 0) {
     printf("%s:%d:FAIL: got \"%s\", expected \"%s\"\n", file, line, actual,
            expected);
-    ret = EXIT_FAILURE;
     return;
   }
   printf("%s:%d:PASS: \"%s\"\n", file, line, actual);
 }
 
+// Caller must free the returned value.
 char *parse(const char *req) {
   struct connection conn;
   conn.request = (char *)req;
@@ -89,5 +85,5 @@ int main(void) {
       free(s);
   }
 
-  return ret;
+  return 0;
 }
